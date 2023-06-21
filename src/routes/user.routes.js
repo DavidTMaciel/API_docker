8a85b0e1469259a606285ef4e98856e85bc5c5bc
DavidTMaciel <davidmacieljr122@gmail.com>
2023-06-21 19:47:32 -0300

    Adionando a rota listar usuarios

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { create } from "../controller/user-controller";
+import { listUsers } from "../controller/user-controller";
 
 const userRoutes = app =>{
     app.post("/user", create)
-
+    app.get("/users", listUsers)
 };
 
 export default userRoutes;
