7cea83833cf8e9df0b2006c8d28ed933b2dbb1c8
DavidTMaciel <davidmacieljr122@gmail.com>
2023-06-21 20:03:30 -0300

    Rota listar usuario por id

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { create } from "../controller/user-controller";
 import { listUsers } from "../controller/user-controller";
+import { listUser } from "../controller/user-controller";
 
 const userRoutes = app =>{
     app.post("/user", create)
     app.get("/users", listUsers)
+    app.get("/user/:id", listUser)
 };
 
 export default userRoutes;
