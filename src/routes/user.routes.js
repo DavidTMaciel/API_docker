4988c0076d026a7ae70471699c842e3dd244b0e5
DavidTMaciel <davidmacieljr122@gmail.com>
2023-07-04 19:16:39 -0300

    router update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { create } from "../controller/user-controller";
 import { listUsers } from "../controller/user-controller";
 import { listUser } from "../controller/user-controller";
+import { update } from "../controller/user-controller";
 
 const userRoutes = app =>{
     app.post("/user", create)
     app.get("/users", listUsers)
     app.get("/user/:id", listUser)
+    app.put("/user/update/:id", update)
 };
 
 export default userRoutes;
