aed3b5f04e0e2e203ee48a81c61b5ec920a99b0d
DavidTMaciel <davidmacieljr122@gmail.com>
2023-07-04 19:23:38 -0300

    route delete user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,11 @@
-import { create } from "../controller/user-controller";
-import { listUsers } from "../controller/user-controller";
-import { listUser } from "../controller/user-controller";
-import { update } from "../controller/user-controller";
+import { create, deleteUserId, listUsers, listUser, update } from "../controller/user-controller";
 
 const userRoutes = app =>{
     app.post("/user", create)
     app.get("/users", listUsers)
     app.get("/user/:id", listUser)
     app.put("/user/update/:id", update)
+    app.delete("/user/delete/:id", deleteUserId)
 };
 
 export default userRoutes;
